670bdda72c8d5d5e8d1acedda774bad1626afe02
nielnat <nielnat@orange.fr>
2022-01-09 13:31:28 +0100

    Modifs 20210109_1

--- mes_repoGIT/HelloWorld/Hello.java
+++ mes_repoGIT/HelloWorld/Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
 	public static void main(String[] args) {
 		for (int i=1;i<=10;i++ ) {
-			System.out.println("Hello world GIT ..."+i);
+			System.out.println("Hello world Nielnat ..."+i);
 		}
 	}
 }
